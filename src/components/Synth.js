c8a04d447219866211a89252a0906decf1379367
pawanbenjamin <pawanbenjamin@gmail.com>
2021-09-05 16:27:59 -0400

    hmmmm maybe context wont work

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { store } from "../state";
+
+import * as Tone from "tone";
 
 function Synth(props) {
   const { state, dispatch } = useContext(store);
 
   console.log(state);
+
+  const synth = new Tone.PolySynth().toDestination();
+  const now = Tone.now();
+  useEffect(() => {
+    state.notes.map((note) => {
+      synth.triggerAttackRelease(note.note, "8n");
+    });
+  });
+
   return <div></div>;
 }
 
